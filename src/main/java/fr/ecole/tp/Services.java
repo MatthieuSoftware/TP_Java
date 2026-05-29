4c35f660d752e737828548a5ed8e84f2dd3ebde6
Matthieu <matthieujeanpierre5@gmail.com>
2026-05-29 17:14:22 +0200

    modification service

--- src/main/java/fr/ecole/tp/Services.java
+++ src/main/java/fr/ecole/tp/Services.java
@@ -6,10 +6,7 @@ import java.util.stream.Collectors;
 
 public class Services {
 
-        public void afficherProduit(List<Produit> produits) {
-            Consumer<Produit> afficherProduit = System.out::println;
-            produits.forEach(afficherProduit);
-        }
+
 
 
 
@@ -156,6 +153,84 @@ public class Services {
                     .map(LigneCommande::getProduit)
                     .forEach(System.out::println);
         }
+
+
+    public void afficherProduit(List<Produit> produits) {
+        produits.forEach(System.out::println);
+    }
+
+
+    //
+    Function<Produit, String> formatProduit = p -> {
+        return "[" + p.getCategorie().toUpperCase() + "] "
+                + p.getNom()
+                + " — "
+                + String.format("%.2f €", p.getPrix());
+    };
+
+    // fonctionalité 3
+    Predicate<Produit> estEnPromotion;
+    Predicate<Produit> prixSuperieurA100;
+    Predicate<Produit> categorieInformatique;
+
+    // fonctionalité 4
+
+    public void filtrage(List<Produit> produits) {
+        produits.stream()
+                .filter(p -> p.getPrix() > 100)
+                .filter(p -> p.isPromotion())
+                .forEach(System.out::println);
+    }
+
+    // fonctionalité 7
+
+    public void catDistinct(List<Produit> produits) {
+        produits.stream()
+                .map(Produit::getCategorie)
+                .distinct()
+                .forEach(System.out::println);
+    }
+
+
+    public void cateToSet(List<Produit> produits) {
+        Set<String> categories = produits.stream()
+                .map(Produit::getCategorie)
+                .collect(Collectors.toSet());
+
+        categories.forEach(System.out::println);
+    }
+
+
+    // fonctionalité 10
+
+    public void reduc(List<Produit> produits) {
+
+        double total =  produits.stream()
+                .map(Produit::getPrix)
+                .reduce (0.0,(a, b) -> a + b);
+    }
+
+    // fonctialité  11
+
+    public void  somme(List<Produit> produits) {
+        double prixTotal = produits.stream()
+                .mapToDouble(Produit::getPrix)
+                .sum();
+        System.out.println("Prix total du toto est -> :"+ prixTotal +"€");
+
+    }
+
+    //fonct 13
+
+
+    public void flatMap(List<Commande> commandes) {
+
+        commandes.stream()
+                .flatMap(c -> c.getLignes().stream())
+                .map(LigneCommande::getProduit)
+                .forEach(System.out::println);
+    }
+
 
 }
 
